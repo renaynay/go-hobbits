7bf5e1d765f92e5511c1b364b290e7af7ed76887
renaynay <renenayman11@gmail.com>
2019-06-13 17:26:18 -0400

    simple test for TCP server

--- tcp/server.go
+++ tcp/server.go
@@ -5,7 +5,6 @@ package tcp
 import (
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"net"
 
 	"github.com/renaynay/go-hobbits/encoding"
@@ -28,28 +27,30 @@ func NewServer(host string, port int) *Server {
 func (s *Server) Listen(c Callback) error {
 	listen, err := net.Listen("tcp", fmt.Sprintf("%s:%d", s.host, s.port))
 	if err != nil {
-		return errors.New(fmt.Sprintf("Error listening: %s", (err.Error())))
+		return errors.New(fmt.Sprintf("Error listening: %s", (err.Error()))) // TODO: errorf, use errors.New when it's a constant (make it public and exported
 	}
 	defer listen.Close()
 
 	for {
-		conn, err := listen.Accept()
+		conn, err := listen.Accept() // TODO: maybe the error is worth a log or an error chan since it stops the routine if you get one error
 		if err != nil {
 			return err
 		}
 
-		go s.handle(conn, c)
+		go handle(conn, c)
 	}
 }
 
 // handle handles incoming requests
-func (*Server) handle(conn net.Conn, c Callback) error {
-	read, err := ioutil.ReadAll(conn)
+func handle(conn net.Conn, c Callback) error {
+	buf := make([]byte, 1024)
+
+	_, err := conn.Read(buf)
 	if err != nil {
 		return errors.New(fmt.Sprintf("Error reading: %s", err.Error())) // TODO: clean up error
 	}
 
-	decoded, err := encoding.Unmarshal(string(read))
+	decoded, err := encoding.Unmarshal(string(buf))
 	if err != nil {
 		return err
 	}
@@ -60,18 +61,15 @@ func (*Server) handle(conn net.Conn, c Callback) error {
 }
 
 // SendMessage sends an encoded message
-func (*Server) SendMessage(conn net.Conn, message encoding.Message) error {
+func (*Server) SendMessage(conn net.Conn, message encoding.Message) error { //TODO: how can this be easier to use? does it need to operate on Server
+	defer conn.Close()
+
 	encoded, err := encoding.Marshal(message)
 	if err != nil {
 		return err
 	}
 
 	_, err = conn.Write([]byte(encoded))
-	if err != nil {
-		return err
-	}
-
-	err = conn.Close()
 	if err != nil {
 		return err
 	}
--- tcp/server_test.go
+++ tcp/server_test.go
@@ -1,15 +1,19 @@
 package tcp
 
 import (
+	"fmt"
+	"net"
 	"reflect"
 	"strconv"
 	"testing"
+
+	"github.com/renaynay/go-hobbits/encoding"
 )
 
 func TestNewServer(t *testing.T) {
 	var test = []struct {
-		host string
-		port int
+		host   string
+		port   int
 		server *Server
 	}{
 		{host: "test", port: 3333, server: &Server{host: "test", port: 3333}},
@@ -26,27 +30,37 @@ func TestNewServer(t *testing.T) {
 	}
 }
 
-// TODO: this isn't a unit-test type test, it's more of an integration test
-// TODO:
-//  1) pipe a channel through handle (do this by creating a chan in callback?)
-//  2) ignore conn
+func TestTCP(t *testing.T) {
+	server := NewServer("", 1123)
+	ch := make(chan encoding.Message)
 
+	go server.Listen(func(_ net.Conn, message encoding.Message) {
+		ch <- message
+	})
 
+	conn, err := net.Dial("tcp", ":1123")
+	if err != nil {
+		t.Error("could not connect to TCP server: ", err)
+	}
 
-//func TestServer_Listen(t *testing.T) {
-//	//TODO: complete test
-//}
-//
-//func Test_handle(t *testing.T) {
-//	var test = []struct {
-//		conn net.Conn
-//		c Callback
-//	}{
-//		{}
-//	}
-//	//TODO: complete test
-//}
-//
-//func TestServer_SendMessage(t *testing.T) {
-//	//TODO: complete test
-//}
+	fmt.Println(conn)
+
+	_, err = conn.Write([]byte("EWP 13.05 RPC blahblahblah json 16 14\nthis is a headerthis is a body"))
+	if err != nil {
+		t.Error("could not write to the TCP server: ", err)
+	}
+	read := <-ch
+
+	expected := encoding.Message{
+		Version:     "13.05",
+		Protocol:    "RPC",
+		Compression: "blahblahblah",
+		Encoding:    "json",
+		Headers:     []byte("this is a header"),
+		Body:        []byte("this is a body"),
+	}
+
+	if !reflect.DeepEqual(expected, read) {
+		t.Error("return value from TCP server does not match expected value")
+	}
+}
