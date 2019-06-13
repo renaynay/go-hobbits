6cdc33f249c13a259aedea64841df3843376bb5f
renaynay <renenayman11@gmail.com>
2019-06-13 12:10:39 -0400

    test for NewServer

--- tcp/server_test.go
+++ tcp/server_test.go
@@ -1,9 +1,29 @@
 package tcp
 
-import "testing"
+import (
+	"reflect"
+	"strconv"
+	"testing"
+)
 
 func TestNewServer(t *testing.T) {
+	var test = []struct {
+		host string
+		port int
+		server *Server
+	}{
+		{host: "test", port: 3333, server: &Server{host: "test", port: 3333}},
+		{host: "host", port: 4000, server: &Server{host: "host", port: 4000}},
+	}
 
+	for i, tt := range test {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			server := NewServer(tt.host, tt.port)
+			if !reflect.DeepEqual(&server, &tt.server) {
+				t.Errorf("return value of NewServer does not match expected value")
+			}
+		})
+	}
 }
 
 func TestServer_Listen(t *testing.T) {
