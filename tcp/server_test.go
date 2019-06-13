797a5e9ed9646ddafa00998d2c5a10d02943e382
renaynay <renenayman11@gmail.com>
2019-06-12 23:50:46 -0400

    test funcs todo

---
+++ tcp/server_test.go
@@ -0,0 +1,19 @@
+package tcp
+
+import "testing"
+
+func TestNewServer(t *testing.T) {
+
+}
+
+func TestServer_Listen(t *testing.T) {
+
+}
+
+func TestServer_SendMessage(t *testing.T) {
+
+}
+
+func Test_send(t *testing.T) {
+
+}
