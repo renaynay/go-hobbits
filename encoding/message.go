45aa49b73bfcaa4bea53c92ec7505160250b6071
renaynay <renenayman11@gmail.com>
2019-06-21 17:25:38 -0400

    enum

--- encoding/message.go
+++ encoding/message.go
@@ -4,11 +4,19 @@
 // By Rene Nayman
 package encoding
 
+type Protocol string
+
+const (
+	RPC Protocol = "RPC"
+	GOSSIP Protocol = "GOSSIP"
+	PING Protocol = "PING"
+)
+
 // Message represents a parsed Hobbits message.
 // See examples of unparsed and parsed messages here: https://github.com/deltap2p/hobbits/blob/master/specs/protocol.md
 type Message struct {
 	Version  string
-	Protocol string
+	Protocol Protocol
 	Header   []byte
 	Body     []byte
 }
